cdc84c5b53e24b56349ad0771f8786cd19509821
Ayamne WSL <elmainiaymane03@gmail.com>
2024-01-21 15:25:07 +0100

    this is the crud tuto on laravel

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $posts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+    return view('home', ["posts" => $posts]);
+});
+Route::post('/register', [\App\Http\Controllers\UserController::class, 'register']);
+Route::post('/logout', [\App\Http\Controllers\userController::class, 'logout']);
+Route::post('/login', [\App\Http\Controllers\userController::class, 'login']);
+
+// post routes
+Route::post("create-post", [PostController::class, "create"]);
+Route::get("/edit-post/{post}", [PostController::class, 'showEditScreen']);
+Route::put("/edit-post/{post}", [PostController::class, 'updatePost']);
+Route::delete("/delete-post/{post}", [PostController::class, 'deletePost']);
